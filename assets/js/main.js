3c41c6af46fccb14a98899fb3ecdfb1f0fca7a55
Kristyn Ginski <missginski@gmail.com>
2020-09-20 15:16:01 -0400

    build out additional pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+/**************** NAV */
 let button = $('.toggle-nav'),
     navbar = $('header');
 
@@ -5,5 +6,15 @@ button.on('click', function() {
   $('.menu').toggleClass('active');
   button.toggleClass('close-nav');
 });
+
+/**************** IMG HOVER */
+// let front = $('.service-card'),
+//     image = $('.service-img-wpr img');
+
+// $(this).on('mouseover', function() {
+//   // console.log('hi')
+//   image.css('transform', 'scale(1.15)');
+//   image.css('transition', '.5s');
+// })
 
 
